6a4bbc018ac223a092df83f53bd792ecbf0e36d8
NSX <83533755+CavDev@users.noreply.github.com>
2022-07-27 14:06:13 +0800

    Create main.py

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import sv_ttk
 import random as rd
 import re
 
+isCheck = True # 用布尔类型检测是否开启完整字符集
+
 class NameList:
+    
+    # Normal
     listA = ("ẠạÅåÄäẢảḀḁẤấẦầẨẩȂȃẪẫẬậẮắẰằẲẳẴẵẶặĀāĄąȀȁǺǻȦȧÁáǞǟǍǎÀàÃãǠǡÂâȺⱥÆæǢǣǼǽⱭ℀⅍℁ª")
     listB = ("ḂḃḄḅḆḇƁɃƀ")
     listC = ("çḈḉĆćĈĉĊċČčÇçƇƈȻȼℂ℃Ɔℭ")
@@ -32,12 +36,41 @@ class NameList:
     listX = ("ẊẋẌẍℵ×")
     listY = ("ẎẏỲỳỴỵỶỷỸỹŶŷƳƴŸÿÝýɎɏȲȳƔẙ")
     listZ = ("ẐẑẒẓẔẕŹźŻżŽžȤȥⱫⱬƵƶɀℨℤ")
+    
+    # Less
+    lessListA = ("ǠǡÂâȺⱥ")
+    lessListB = ("ḂḃḄḅḆḇƁɃƀ")
+    lessListC = ("ÇçƇƈȻȼℂ℃ℭ")
+    lessListD = ("ƊƉđȡ")
+    lessListE = ("ȨȩȆȇɆɇℯ℮")
+    lessListF = ("ḞḟƑƒℱ")
+    lessListG = ("ǧǴℊǵĢģ")
+    lessListH = ("ℍℏℎℋ")
+    lessListI = ("ḬḭǏǐıƚ")
+    lessListJ = ("ĴĵɈɉȷǰ")
+    lessListK = ("ḲḳḴḵĶƘƙⱩⱪĸ")
+    lessListL = ("ỊİịȈȉȊȋℓℒ")
+    lessListM = ("ḾḿṀṁṂṃℳ")
+    lessListN = ("ȠƞŊŋƝŉȵℕ№")
+    lessListO = ("ȌȍȎȏØøǾǿ⍥⍤")
+    lessListP = ("ṗƤƥⱣℙǷ℘")
+    lessListQ = ("Ɋɋℚȹ")
+    lessListR = ("ŖŗɌɍƦⱤ℞ℜℛ℟ℝ")
+    lessListS = ("ŜŝŞşŠšȘșȿ")
+    lessListT = ("ŦŧȚțȾⱦƬƮƫƭẗȶ")
+    lessListU = ("ṳƯưɄƲƱ")
+    lessListV = ("ṾṿɅ℣ⱱⱴ")
+    lessListW = ("ŴŵⱲⱳϢϣẘ")
+    lessListX = ("Ẋℵ×✗✘ㄨ✕")
+    lessListY = ("ɎɏȲȳƔẙ")
+    lessListZ = ("ȤȥⱫⱬƵƶɀℤ")
 
 class Event:
     # def __init__(self, e):
     #     self.e = StringVar
 
     def clickButton(event):
+        global isCheck
         textList = ""
         resultTextList = ""
         overText = ""
@@ -49,18 +82,22 @@ class Event:
             for index in range(len(e.get())):
                 x = e.get()[index]
                 result = re.match("[A-Za-z]*", x) # 用正则表达式匹配26个英文字母
-                # messagebox.showinfo("1", result.group()) 
                 textList += str(result.group())
                
         resultTextList = textList
         
-        for index in range(len(resultTextList)):
-            indexZ = len(eval("NameList.list" + resultTextList[index].upper()))
-            indexDown = rd.randint(0, indexZ)
-            # messagebox.showinfo("title", indexDown)   
-            overText += eval("NameList.list" + resultTextList[index].upper())[indexDown - 1]
-
-        # messagebox.showinfo("title", overText)   
+        if isCheck == False: # 当“完整字符集”未被勾选
+             for index in range(len(resultTextList)):
+                 indexZ = len(eval("NameList.list" + resultTextList[index].upper()))
+                 indexDown = rd.randint(0, indexZ)
+                 # messagebox.showinfo("title", indexDown)   
+                 overText += eval("NameList.list" + resultTextList[index].upper())[indexDown - 1]
+        elif isCheck == True:
+            for index in range(len(resultTextList)):
+                 indexZ = len(eval("NameList.lessList" + resultTextList[index].upper()))
+                 indexDown = rd.randint(0, indexZ)
+                 overText += eval("NameList.lessList" + resultTextList[index].upper())[indexDown - 1]
+          
         e.set(overText)
 
     def changeTheme(event):
@@ -68,8 +105,16 @@ class Event:
         sv_ttk.toggle_theme()
 
     def aboutThis(event):
-        messagebox.showinfo("说明", "这是一个写了两小时的小工具\n它可以为你将你原来输入的英文字母使用特殊符号代替\n输入的其它字符会被忽略，所以你可以直接把你的名字复制进来\n你可以将它用作名字或是任何途径\n(PS:请手动 Ctrl + C 复制文本框中内容)\n\nBy NSX\n2022.7.25")
+        messagebox.showinfo("说明", "又是一个牛马工具\n它可以为你将你原来输入的英文字母使用特殊符号代替\n输入的其它字符会被忽略，所以你可以直接把你的名字复制进来\n你可以将它用作名字或是任何途径\n如果想要使用更多字符来进行匹配请勾选 “完整字符集” 选项'\n(PS:请手动 Ctrl + C 复制文本框中内容)\n\nBy NSX\n2022.7.25")
 
+    def changeCharsType():
+        global isCheck
+        
+        if isCheck == 0:
+            isCheck = True
+        else:
+            isCheck = False
+    
 class Win:
     def __init__(self):
         self.root = self.__win()
@@ -78,6 +123,7 @@ class Win:
         self.tk_input_nameBox = self.__tk_input_nameBox()
         self.tk_button_changeTheme = self.__tk_button_changeTheme()
         self.tk_button_aboutThis = self.__tk_button_aboutThis()
+        self.tk_Checkbutton_fullChars = self.__tk_Checkbutton_fullChars()
 
     def __win(self):
         root = Tk()
@@ -119,12 +165,18 @@ class Win:
         btn.place(x=430, y=220, width=85, height=36)
         btn.bind('<Button-1>', Event.aboutThis)
         return btn
+    
+    def __tk_Checkbutton_fullChars(self):
+        swt = Checkbutton(self.root, style="Switch.TCheckbutton", text='完整字符集', command=Event.changeCharsType)
+        swt.place(x=410, y=82, width=180, height=36)
+        return swt
 
     def __tk_input_nameBox(self):
         global e
         e = StringVar()
         ipt = Entry(self.root, textvariable=e)
         ipt.place(x=80, y=150, width=459, height=37)
+        ipt.bind('<Return>', Event.clickButton) # 绑定到回车
         return ipt
 
 if __name__ == "__main__":
